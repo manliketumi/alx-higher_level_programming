2cc0833f101f877dd0935bbcceae7dc799fde902
manliketumi <mongwegeloa@gmail.com>
2023-11-29 03:16:08 -0800

    comb3

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i < 99:
-        print(f"{i:02d}", end=", ")
-    else:
-        print(f"{i:02d}")
+for first_number in range(0, 10):
+
+    for second number in range(first_number + 1, 10):
+        if first number == 8 and second number == 9:
+             print(“{}{}”.format(first_number, second_number))
+        else:
+             print(“{}{}”.format(first_number, second_number), end=‘’)
