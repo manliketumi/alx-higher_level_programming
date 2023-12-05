a5f6ee9fc8bafedd9f0f587eba80235361c9f480
manliketumi <mongwegeloa@gmail.com>
2023-12-04 23:45:46 -0800

    nultiple returns

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,11 +3,11 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         print()
     else:
-        for row in range (len(matrix)):
-             for item in range (len(matrix[row])):
-                 if item != len(matrix[row]) - 1:
-                    endspace = ''
-                 else:
-                     endspace = ''
-                 print("{:d}".format(matrix[row[item]), end=endspace)
-             print()
+        for row in range(len(matrix)):
+           for item in range(len(matrix[row])):
+               if item != len(matrix[row]) - 1:
+                   endspace = ''
+               else:
+                   endspace = ''
+                   print("{:d}".format(matrix[row[item]), end=endspace)
+           print()
