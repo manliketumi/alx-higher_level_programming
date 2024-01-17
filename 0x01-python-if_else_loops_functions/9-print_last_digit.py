99adb0c01ac83d02f91ad064648ffe66c24ee319
manliketumi <mongwegeloa@gmail.com>
2024-01-17 05:30:00 -0800

    last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 def print_last_digit(number):
-    
+    if number < 0:
+        number = -number
+    last_digit = number % 10
+    print("{}".format(last_digit), end='')
+    return (last_digit)
