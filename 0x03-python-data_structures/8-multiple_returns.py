d6ee26f94dc51eb45103c4ee40802697c0c30ada
manliketumi <mongwegeloa@gmail.com>
2024-01-23 00:33:30 -0800

    try again

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -7,4 +7,4 @@ def multiple_returns(sentence):
     else:
         sen_len = 0
 
-    return(sen_len, sentence if not sentence else sentence[:1])
+    return (sen_len, sentence if not sentence else sentence[:1])
