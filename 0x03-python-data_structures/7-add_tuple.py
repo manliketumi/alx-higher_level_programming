f5d4c27cb2663a17a8e5d4f6e06356ff87a6dc1f
manliketumi <mongwegeloa@gmail.com>
2024-01-22 23:52:38 -0800

    two tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b()):
-    len_a, len_b = len(tuple_a), len(tuple_b)
-    new_tuple = ((tuple_a[0] if len_a >= 1 else 0) + 
-            (tuple_b[0] if len_b >= 1 else 0),
-            (tuple_a[1] if len_a >= 2 else 0) + 
-            (tuple_b[1] if len_b >= 2 else 0))
-
-return new_tuple
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 1:
+        tuple_a = (tuple_a[0], 0)
+    elif len(tuple_a) == 0:
+        tuple_a = (0, 0)
+    # t-b
+    if len(tuple_b) == 1:
+        tuple_b = (tuple_b[0], 0)
+    elif len(tuple_b) == 0:
+        tuple_b = (0, 0)
+    # a_o
+    tuple_sum = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return (tuple_sum)
--- 0x03-python-data_structures/7-main.py
+++ 0x03-python-data_structures/7-main.py
@@ -7,4 +7,5 @@ new_tuple = add_tuple(tuple_a, tuple_b)
 print(new_tuple)
 
 print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ())
+print(add_tuple(tuple_a, ()))
+
