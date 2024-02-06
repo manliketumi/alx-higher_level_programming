d89f15668266ae3bc880674800d87f63c90fa4ae
manliketumi <mongwegeloa@gmail.com>
2024-02-06 09:59:10 -0800

    debug

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,39 +3,49 @@
 
 
 class Square:
-    """Represent a class square."""
+    """ A class that defines a square by its size
+    """
 
-    def __init__(self, size):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
+    def __init__(self, size=0):
+        """ Method to initialize the square object
         """
-        self.size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
-        @property
-        def size(self):
-            """Get/set the current size of the square."""
-            return (self.__size)
+    def area(self):
+        """ Method that returns the square are of the object
+        """
+        return (self.__size ** 2)
 
-        @size.setter
-        def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
+    @property
+    def size(self):
+        """ Method to returns the size value
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Method to set the size value of the square object
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
             self.__size = value
 
-        def area(self):
-            """Return the current area of the square."""
-            return (self.__size * self.__size)
-
-        def my_print(self):
-            """Print the square with the # character."""
-            if not self.__size:
+    def my_print(self):
+        """ Method that prints a # square according
+        to the size value
+        """
+        if not self.__size:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
                 print()
-            else:
-                for i in range(self.__size):
-                    for j in range(self.__size):
-                        print("#", end='')
-                    print()
